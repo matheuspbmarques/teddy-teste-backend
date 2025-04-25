b443878e45006a79214a58267da5cd97940fea93
Matheus Marques <matheuspbmarques@protonmail.com>
2025-04-25 01:36:53 -0300

    add: sign-in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateUserDto } from "src/user/dto/create-user.dto";
 import { AuthService } from "./auth.service";
+import { SignInAuthDto } from "./dto/sign-in-auth.dto";
+import { AuthTokensResponse } from "./auth.interface";
 
 @Controller('auth')
 export class AuthController {
-    constructor (private authService: AuthService) {};
+    constructor(private authService: AuthService) { };
 
     @Post('sign-up')
-    async signUp (@Body() data: CreateUserDto) {
+    async signUp(@Body() data: CreateUserDto): Promise<AuthTokensResponse> {
         return this.authService.signUp(data);
+    };
+
+    @Post('sign-in')
+    async signIn(@Body() data: SignInAuthDto): Promise<AuthTokensResponse> {
+        return this.authService.signIn(data);
     };
 };
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { CreateUserDto } from "src/user/dto/create-user.dto";
 import { UserService } from "src/user/user.service";
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from "@nestjs/config";
+import { PrismaService } from "src/prisma/prisma.service";
+import { SignInAuthDto } from "./dto/sign-in-auth.dto";
+import { AuthTokensResponse } from "./auth.interface";
 
 @Injectable()
 export class AuthService {
     constructor(
         private userService: UserService,
         private jwtService: JwtService,
-        private configService: ConfigService
+        private configService: ConfigService,
+        private prisma: PrismaService
     ) { };
 
-    async signUp({ name, email, password }: CreateUserDto) {
+    private async generateTokens (userId: string): Promise<AuthTokensResponse> {
+        const secret = this.configService.get<string>('JWT_SECRET');
+
+        const [accessToken, refreshToken] = await Promise.all([
+            this.jwtService.signAsync({ sub: userId }, { expiresIn: '12h', secret: secret }),
+            this.jwtService.signAsync({ sub: userId }, { expiresIn: '7d', secret: secret })
+        ]);
+
+        return { accessToken, refreshToken };
+    };
+
+    async signUp({ name, email, password }: CreateUserDto): Promise<AuthTokensResponse> {
         const salt = await bcrypt.genSalt();
         const passwordHashed = await bcrypt.hash(password, salt);
 
@@ -28,13 +43,30 @@ export class AuthService {
         return { accessToken, refreshToken };
     };
 
-    async generateTokens (userId: string) {
-        const secret = this.configService.get<string>('JWT_SECRET');
+    async signIn({ email, password }: SignInAuthDto): Promise<AuthTokensResponse> {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email
+            }
+        });
 
-        const [accessToken, refreshToken] = await Promise.all([
-            this.jwtService.signAsync({ sub: userId }, { expiresIn: '12h', secret: secret }),
-            this.jwtService.signAsync({ sub: userId }, { expiresIn: '7d', secret: secret })
-        ]);
+        if (!user) {
+            throw new HttpException({
+                error: 'USER-NOT-FOUND',
+                message: 'O usuário não encontrado'
+            }, HttpStatus.NOT_FOUND);
+        };
+
+        if (!await bcrypt.compare(password, user.password)) {
+            throw new HttpException({
+                error: 'PASSWORD-WRONG',
+                message: 'A senha informada é invalida'
+            }, HttpStatus.UNAUTHORIZED);
+        };
+
+        const { accessToken, refreshToken } = await this.generateTokens(user.id);
+
+        await this.userService.updateRefreshToken(user.id, refreshToken);
 
         return { accessToken, refreshToken };
     };
